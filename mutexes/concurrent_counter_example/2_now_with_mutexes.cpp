f7a2284b0a755f06877789a3bc39d1ada1c9d95f
Sergey Kosykh <s3rkosal@gmail.com>
2026-07-10 21:49:37 +0400

    added example with mutexes

---
+++ mutexes/concurrent_counter_example/2_now_with_mutexes.cpp
@@ -0,0 +1,28 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <print>
+
+int counter = 0;
+std::mutex counter_mutex;
+
+void add(int n) {
+    std::println("thread id = {}", std::this_thread::get_id());
+    for (int i = 0; i < n; ++i) {
+        counter_mutex.lock();
+        ++counter;
+        counter_mutex.unlock();
+    }
+}
+
+int main() {
+    std::locale::global(std::locale(""));
+
+    std::thread t(add, 1'000'000);
+    add(1'000'000);
+    t.join();
+
+    std::println("the value of the counter is: {:L}", counter);
+
+    return 0;
+}
